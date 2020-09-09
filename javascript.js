21bf70214093318f7afe12a3b7d8469cc3ed905b
Aseem <aseem.tim@gmail.com>
2020-09-09 13:28:26 +0545

    Added hover effect and clear function

--- javascript.js
+++ javascript.js
@@ -1,9 +1,25 @@
 const container = document.querySelector('#container');
 
 let counter = 1;
+let content;
 while (counter <= 256) {
-    const content = document.createElement('div');
+    content = document.createElement('div');
     content.classList.add('theDiv');
     container.appendChild(content);
     counter ++;
 }
+
+const allDiv = document.querySelectorAll('.theDiv');
+allDiv.forEach(div => {
+    div.addEventListener('mouseover', function (e) {
+        e.srcElement.classList.add('hover');
+    })
+});
+
+const clearBoard = document.querySelector('.clearBoard');
+clearBoard.addEventListener('click', () => {
+    const removeClass = document.querySelectorAll('.theDiv');
+    removeClass.forEach(div => {
+        div.classList.remove('hover');
+    })
+})
